861ce6eb2d69aeef2bb6a609b776b637add3ac20
isaacle77 <97554746+isaacle77@users.noreply.github.com>
2022-10-04 09:58:38 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,13 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyD063WTRRp9hqHDrry0LRq95pNu3zqpG94",
-  authDomain: "bicicleta-eletronica.firebaseapp.com",
-  projectId: "bicicleta-eletronica",
-  storageBucket: "bicicleta-eletronica.appspot.com",
-  messagingSenderId: "777022380523",
-  appId: "1:777022380523:web:85da7254f92ee85c5be1ae"
+   apiKey: "AIzaSyADumbLIsXZMUQvB2Ytsv5u56E8sb_IXUU",
+  authDomain: "pro-71-bicicleta.firebaseapp.com",
+  databaseURL: "https://pro-71-bicicleta-default-rtdb.firebaseio.com",
+  projectId: "pro-71-bicicleta",
+  storageBucket: "pro-71-bicicleta.appspot.com",
+  messagingSenderId: "375702355936",
+  appId: "1:375702355936:web:86c0adf69165490ee77585"
 };
 
 firebase.initializeApp(firebaseConfig);
